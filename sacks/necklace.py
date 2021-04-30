69a3230ba008f89695cda2d370b4e34c3cc61a44
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-29 21:52:18 -0500

    Updated docstring.

--- sacks/necklace.py
+++ sacks/necklace.py
@@ -1,6 +1,7 @@
 class Necklace:
     """
-    `Necklace`s are immutable sequences that "wrap-around".
+    `Necklace`s are immutable sequences that "wrap-around".  Two necklaces `n`, `m` are equal if `n == m[i:] + m[:i]` for some `i`.
+    (see https://en.wikipedia.org/wiki/Necklace_(combinatorics) )
 
     Because `Necklaces` wrap, `__getitem__` slices will also wrap.
 
