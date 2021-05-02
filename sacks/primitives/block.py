7e5ce3ef01e1bb45bcf851ab4523e90e868e8c23
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-01 20:27:43 -0500

    Refactor: simpler implementation of `Block` using dancing link methods.

--- sacks/primitives/block.py
+++ sacks/primitives/block.py
@@ -1,38 +1,33 @@
 class Block:
     """Primitive element of a doubly-linked list.
     """
-    __slots__ = 'val', '_prev', '_next'
+    __slots__ = 'val', 'prev', 'next'
 
     def __init__(self, val=None, prev=None, next=None):
         self.val = val
         self.prev = prev or self
         self.next = next or self
 
-    @property
-    def prev(self):
-        return self._prev
+        self.insert()
 
-    @prev.setter
-    def prev(self, block):
-        self._prev = block
-        block._next = self
+    # remove and insert together implement "dancing links" [https://en.wikipedia.org/wiki/Dancing_Links]
+    def remove(self):
+        """Remove this block from its list.
+        """
+        self.prev.next = self.next
+        self.next.prev = self.prev
 
-    @property
-    def next(self):
-        return self._next
-
-    @next.setter
-    def next(self, block):
-        self._next = block
-        block._prev = self
+    def insert(self):
+        """Insert this block into its list.
+        """
+        self.prev.next = self.next.prev = self
 
     def pop(self):
-        """Remove this block from its list by linking its neighbors, then return its `val`.
+        """Remove this block from its list, de-reference its links, and return its `val`.
         """
-        self.prev._next = self.next
-        self.next._prev = self.prev
+        self.remove()
 
-        # Remove possible circular reference
-        self._next = self._prev = None
+        # Remove possible circular references keeping this object alive
+        self.next = self.prev = None
 
         return self.val
