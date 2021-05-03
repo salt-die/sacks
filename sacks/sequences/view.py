d3871ef52a7c483214642f31f2ae0d4a5909dc54
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-03 02:53:29 -0500

    `range` made private as it can be a source of errors. A warning about passing that parameter added to docstring.

--- sacks/sequences/view.py
+++ sacks/sequences/view.py
@@ -2,52 +2,57 @@ from collections.abc import MutableSequence
 
 
 class View(MutableSequence):
-    """A mutable view of a sequence.
     """
-    __slots__ = 'sequence', 'range',
+    A mutable view of a sequence.
 
-    def __init__(self, sequence, range=None):
+    Warning
+    -------
+    Passing in a `_range` is not recommended.  Better to slice a default `View` instead.
+    """
+    __slots__ = 'sequence', '_range',
+
+    def __init__(self, sequence, _range=None):
         self.sequence = sequence
-        self.range = range or __builtins__['range'](len(sequence))
+        self._range = _range or range(len(sequence))
 
     def insert(self, index, item):
         """Insert item before index.
         """
-        self.sequence.insert(self.range[index], item)
+        self.sequence.insert(self._range[index], item)
 
     def __getitem__(self, key):
         if isinstance(key, slice):
-            return View(self.sequence, self.range[key])
+            return View(self.sequence, self._range[key])
 
-        return self.sequence[self.range[key]]
+        return self.sequence[self._range[key]]
 
     def __setitem__(self, key, value):
         if isinstance(key, slice):
-            r = self.range[key]
+            r = self._range[key]
             self.sequence[r.start: r.stop: r.step] = value
         else:
-            self.sequence[self.range[key]] = value
+            self.sequence[self._range[key]] = value
 
     def __delitem__(self, key):
         if isinstance(key, slice):
-            r = self.range[key]
+            r = self._range[key]
             del self.sequence[r.start: r.stop: r.step]
         else:
-            del self.sequence[self.range[key]]
+            del self.sequence[self._range[key]]
 
     def __len__(self):
-        return sum(1 for _ in self)  # len(self.range) isn't exactly correct as range.end might be larger than sequence
+        return len(self._range)
 
     def __iter__(self):
-        for i in self.range:
+        for i in self._range:
             yield self.sequence[i]
 
     def __reversed__(self):
-        for i in reversed(self.range):
+        for i in reversed(self._range):
             yield self.sequence[i]
 
     def __repr__(self):
-        return f'{type(self).__name__}(sequence={self.sequence!r}, range={self.range!r})'
+        return f'{type(self).__name__}(sequence={self.sequence!r}, _range={self._range!r})'
 
     def __str__(self):
         return f'{type(self.sequence)(self)!r}'
