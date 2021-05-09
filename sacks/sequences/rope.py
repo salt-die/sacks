ddca68b7f6d43abebcffb0e48df255323b0cf268
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-09 05:54:41 -0500

    Basic working implementation -- efficient methods still need to be found.

--- sacks/sequences/rope.py
+++ sacks/sequences/rope.py
@@ -30,14 +30,34 @@ class Rope(MutableSequence):
         self._root = RopeInternal()
         self.leafsize = leafsize
         self.type = type or __builtins__['type'](sequence)
-        self._len = 0
+        self._len = len(sequence)
 
         if sequence:
-            raise NotImplementedError
+            self._from_sequence(sequence, self._root)
+            self._collapse()
+            self.balance()
+
+    def _from_sequence(self, sequence, root):
+        size = len(sequence)
+        if size <= self.leafsize:
+            root.left = RopeLeaf(sequence)
+        elif size <= self.leafsize * 2:
+            root.left = RopeLeaf(sequence[:size//2])
+            root.right = RopeLeaf(sequence[size//2:])
+        else:
+            root.left = RopeInternal()
+            root.right = RopeInternal()
+            self._from_sequence(sequence[:size//2], root.left)
+            self._from_sequence(sequence[size//2:], root.right)
 
     @property
     def sequence(self):
         return ''.join(self._root) if self.type is str else sum(self._root)
+
+    def copy(self):
+        copy = Rope(leafsize=self.leafsize, type=self.type)
+        copy._root = self._root.copy()
+        return copy
 
     def __len__(self):
         return self._len
@@ -82,49 +102,116 @@ class Rope(MutableSequence):
         pivot.left = root
         return pivot
 
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            if key >= len(self):
+    def __iter__(self):
+        for seq in self._root:
+            yield from seq
+
+    def _normalize_index(self, index):
+        """Common functionality for parsing slices for __getitem__, __setitem__, and __delitem__.
+        """
+        if isinstance(index, int):
+            if index > len(self):
                 raise IndexError('index out of range')
+            start, stop, step = index, index + 1, 1
+        else:
+            start, stop, step = index.indices(len(self))
 
-            node, j = self._root.query(key)
-            return node.sequence[j]
+        if step != 1:
+            raise ValueError('invalid step')
 
-    def __setitem__(self, key, value):
-        raise NotImplementedError
+        if start >= stop:
+            raise ValueError('0-length slice not supported')
+
+        return start, stop - start
+
+    def __getitem__(self, key):
+        first_split, second_split = self._normalize_index(key)
+
+        #FIXME: This is inefficient
+        _, other = self.split(first_split)
+        _, end = other.split(second_split)
+
+        sequence = other.sequence
+        self.join(other)
+        self.join(end)
+        return sequence
+
+    def __setitem__(self, key, sequence):
+        first_split, second_split = self._normalize_index(key)
+
+        self._len -= second_split + len(sequence)
+
+        _, other = self.split(first_split)
+        _, end = other.split(second_split)
+        self.join(Rope(sequence, leafsize=self.leafsize))
+        self.join(end)
 
     def __delitem__(self, key):
-        raise NotImplementedError
+        first_split, second_split = self._normalize_index(key)
+
+        self._len -= second_split
+
+        _, other = self.split(first_split)
+        _, end = other.split(second_split)
+        self.join(end)
 
     def __add__(self, other):
         if self.type != other.type:
             raise TypeError(f'Incompatible types: {self.type}, {other.type}')
 
         new_rope = Rope(leafsize=max(self.leafsize, other.leafsize), type=self.type)
-        new_rope._root.left = self._root.copy()
-        new_rope._root.right = other._root.copy()
-        new_rope.balance()
+        new_rope._root = self._root.copy()
+        new_rope += other
+        new_rope._len = len(self) + len(other)
         return new_rope
 
     def __iadd__(self, other):
+        self.join(other.copy())
+        self._len += len(other)
+        return self
+
+    def append(self, sequence):
+        self += Rope(sequence, leafsize=self.leafsize)
+        self._len += len(sequence)
+
+    def join(self, other):
         if self.type != other.type:
             raise TypeError(f'Incompatible types: {self.type}, {other.type}')
 
-        self._root = RopeInternal(self._root, other._root.copy())
-        self.balance()
+        self._len += len(other)
 
-    def join(self, other):
+        # FIXME: This is inefficient
         self._root = RopeInternal(self._root, other._root)
         self.balance()
+        # if self._root.height == other._root.height:
+        #     self._root = RopeInternal(self._root, other._root)
+        # elif self._root.height > other._root.height:
+        #     self._join_right(self, other)
+        # else:
+        #     self._join_left(self, other)
+
+    def _join_right(self, other):
+        raise NotImplementedError
+
+    def _join_left(self, other):
+        raise NotImplementedError
 
     def insert(self, index, sequence):
+        self._len += len(sequence)
         _, end = self.split(index)
-        self.join(self, Rope(sequence, leafsize=self.leafsize))
-        self.join(self, end)
-        self.balance()
+        self.join(Rope(sequence, leafsize=self.leafsize))
+        self.join(end)
 
     def split(self, index):
-        raise NotImplementedError
+        right = Rope(leafsize=self.leafsize)
+        right._root = self._root.split(index)
+        right.balance()
+        self.balance()
+
+        right._len = self._len - index
+        self._len = index
+
+        return self, right
 
     def __repr__(self):
         return f'{type(self).__name__}({self.sequence!r}, leafsize={self.leafsize}, type={self.type.__name__})'
