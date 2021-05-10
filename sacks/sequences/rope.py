4b854e1d9dae409ac4110ff7ff1efa75577f9c70
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-10 00:01:40 -0500

    Unused import removed.

--- sacks/sequences/rope.py
+++ sacks/sequences/rope.py
@@ -1,6 +1,5 @@
 # TODO: Keep track of how "messy" a tree is so we can coalesce many short leaves and rebalance.
 from collections.abc import MutableSequence
-from functools import wraps
 
 from ..primitives import RopeInternal, RopeLeaf
 
