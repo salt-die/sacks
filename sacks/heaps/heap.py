a8c17f69259423bb10bd75f4f41d71d4d885b422
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-13 04:56:25 -0500

    `__len__` implemented.

--- sacks/heaps/heap.py
+++ sacks/heaps/heap.py
@@ -3,6 +3,10 @@ from collections.abc import Sized
 
 
 class Heap(ABC, Sized):
+
+    def __len__(self):
+        return self._size
+
     @abstractmethod
     def heappop(self):
         """Pop the smallest item off the heap, maintaining the heap invariant.
