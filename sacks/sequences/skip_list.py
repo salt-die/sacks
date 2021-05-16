a8f521774a4b98c3eaab13e9d1a9012aae0d4605
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-16 03:22:19 -0500

    `_path_to` method added.
    `random_level` moved inside class.

--- sacks/sequences/skip_list.py
+++ sacks/sequences/skip_list.py
@@ -1,60 +1,66 @@
 from collections.abc import MutableSet
 from ..primitives import SkipListBlock
 
-P = .5
-
-def random_level(max_level):
-        level = 1
-        while random() < P and level < max_level:
-            level += 1
-        return level
-
 
 class SkipList(MutableSequence):
-    """An ordered sequence with O(log n) search and insertion.
     """
-    __slots__ = 'head', 'levels', '_resize_len', '_len',
+    An ordered sequence with O(log n) search and insertion.
 
-    def __init__(self, iterable=()):
-        self.head = SkipListBlock(None)
+    Parameters
+    ----------
+    p:
+        Probability a block is linked on the next level. (default: .5)
+    """
+    __slots__ = '_level', '_head', '_tail', '_path', '_resize_len', '_len',
 
-        self.levels = 0
-        self._resize_len = 1
+    def __init__(self, iterable=(), *, p=.5):
+        self._level = 0
         self._len = 0
+        self._resize_len = 1
+
+        self.p = p
+
+        self._head = SkipListBlock(None)
+        self._tail = SkipListBlock(None)
+        self._head.skip = self._tail.skip = 0
+        self._head.forwards.append(self._tail)
+
+    def _random_level(self):
+        level = 1
+        while level < self._level and random() < self.p:
+            level += 1
+        return level
 
     def __len__(self):
         return self._len
 
-    @property
-    def len(self):
-        return self._len
+    def _path_to(self, block):
+        """Generate a path of blocks through the skip list.
+        """
+        node = self._head
+        skip = 0
+        for i in reversed(range(node.height)):
+            next_node = node.forwards[i]
+            while next_node is not self._tail and next_node < block:
+                node, next_node = next_node, next_node.forwards[i]
+                skip += node.skip
+            yield node, skip
 
-    @len.setter
-    def len(self, value):
-        self._len = value
-        self._resize()
-
-    def _resize(self):
+    def add(self, item):
+        self._len += 1
         if self._len >= self._resize_len:
-            self.levels += 1
-            self._resize_len = int((1 / P) ** self.levels)
+            self._level += 1
+            self._head.forwards.append(self._tail)
+            self._tail.skip = len(self)
+            self._resize_len = round((1 / self.p) ** self._level)
+
+        block = SkipListBlock(item, self._random_level())
+
+    def discard(self, item):
+        raise NotImplementedError
 
     def __iter__(self):
         raise NotImplementedError
 
     def contains(self, item):
-        raise NotImplementedError
-
-    def add(self, item):
-        raise NotImplementedError
-        #     self.countdown -= 1
-        #     self._len += 1
-
-        #     next_block = self.head
-        #     while True:
-        #         for level in next_block.forwards:
-        #             if level.value < item:
-        #                 next_block = level
-
-    def discard(self, item):
         raise NotImplementedError
