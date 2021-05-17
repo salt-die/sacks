ea4d53b759fc849fe94cd9bc10a55143c98a7354
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-17 04:46:07 -0500

    Renamed attributes and removed __str__ method.

--- sacks/primitives/skip_list_block.py
+++ sacks/primitives/skip_list_block.py
@@ -1,25 +1,19 @@
-from ._tree_printer import tree_printer
-
-
 class SkipListBlock:
     """Primitive of a Skip List.  Each block is singly-linked to several other blocks.
     """
-    __slots__ = 'value', 'forwards', 'skip',
+    __slots__ = 'value', 'forward_links', 'skips',
 
-    def __init__(self, value):
+    def __init__(self, value, forward_links, skips):
         self.value = value
-        self.forwards = [ ]
+        self.forward_links = forward_links
+        self.skips = skips
 
     @property
-    def height(self):
-        return len(self.forwards) - 1
+    def max_level(self):
+        return len(self.forward_links)
 
-    def __lt__(self, other):
-        return self.value < other.value
+    def __le__(self, other):
+        return self.value <= other.value
 
     def __repr__(self):
         return f'{type(self).__name__}(value={self.value})'
-
-    def __str__(self):
-        return '\n'.join(tree_printer(repr(self), list(map(repr, self.forwards))))
-
