a26ff8f05501961f39cb1b86165ddfe082e542f8
salt-die <53280662+salt-die@users.noreply.github.com>
2021-05-17 04:48:26 -0500

    `add` implementation working.
    Pretty print implemented.

--- sacks/sequences/skip_list.py
+++ sacks/sequences/skip_list.py
@@ -1,8 +1,13 @@
 from collections.abc import MutableSet
+from random import random
+
 from ..primitives import SkipListBlock
+from ..primitives._sentinel import sentinel
+
+TAIL = sentinel('Tail', repr='TAIL', methods={ '__le__': lambda self, other: False })
 
 
-class SkipList(MutableSequence):
+class SkipList(MutableSet):
     """
     An ordered sequence with O(log n) search and insertion.
 
@@ -11,56 +16,90 @@ class SkipList(MutableSequence):
     p:
         Probability a block is linked on the next level. (default: .5)
     """
-    __slots__ = '_level', '_head', '_tail', '_path', '_resize_len', '_len',
+    __slots__ = '_len', 'p', '_head',
 
     def __init__(self, iterable=(), *, p=.5):
-        self._level = 0
         self._len = 0
-        self._resize_len = 1
-
+        self._head = SkipListBlock('HEAD', [ TAIL ], [ 1 ])
         self.p = p
 
-        self._head = SkipListBlock(None)
-        self._tail = SkipListBlock(None)
-        self._head.skip = self._tail.skip = 0
-        self._head.forwards.append(self._tail)
-
-    def _random_level(self):
-        level = 1
-        while level < self._level and random() < self.p:
-            level += 1
-        return level
+    @property
+    def max_level(self):
+        return len(self._head.forward_links)
 
     def __len__(self):
         return self._len
 
-    def _path_to(self, block):
-        """Generate a path of blocks through the skip list.
-        """
-        node = self._head
-        skip = 0
-        for i in reversed(range(node.height)):
-            next_node = node.forwards[i]
-            while next_node is not self._tail and next_node < block:
-                node, next_node = next_node, next_node.forwards[i]
-                skip += node.skip
-            yield node, skip
+    def _random_level(self):
+        level = 1
+        while random() < self.p:
+            level += 1
+        return level
 
     def add(self, item):
         self._len += 1
-        if self._len >= self._resize_len:
-            self._level += 1
-            self._head.forwards.append(self._tail)
-            self._tail.skip = len(self)
-            self._resize_len = round((1 / self.p) ** self._level)
 
-        block = SkipListBlock(item, self._random_level())
+        levels = self._random_level()
+        new_block = SkipListBlock(item, [ None ] * levels, [ None ] * levels)
+
+        # Add new levels to self._head if needed.
+        if levels > self.max_level:
+            new_levels = levels - self.max_level
+            self._head.forward_links.extend(TAIL for _ in range(new_levels))
+            self._head.skips.extend(self._len for _ in range(new_levels))
+
+        # Create a path through the skip list to new_block.
+        path = [ None ] * self.max_level
+        skips = [ 0 ] * self.max_level
+        node = self._head
+        for level in reversed(range(self.max_level)):
+            while node.forward_links[level] <= new_block:
+                skips[level] += node.skips[level]
+                node = node.forward_links[level]
+            path[level] = node
+
+        # Update pointers and skips.
+        total_skip = 0
+        for level in range(levels):
+            previous = path[level]
+            # Pointers
+            new_block.forward_links[level] = previous.forward_links[level]
+            previous.forward_links[level] = new_block
+            # Skips
+            new_block.skips[level] = previous.skips[level] - total_skip
+            previous.skips[level] = total_skip + 1
+            total_skip += skips[level]
+
+        # More skips.
+        for level in range(levels, self.max_level):
+            path[level].skips[level] += 1
 
     def discard(self, item):
         raise NotImplementedError
 
     def __iter__(self):
+        current = self._head.forward_links[0]
+        while current is not TAIL:
+            yield current.value
+            current = current.forward_links[0]
+
+    def __contains__(self, item):
         raise NotImplementedError
 
-    def contains(self, item):
-        raise NotImplementedError
+    def __repr__(self):
+        return f'{type(self).__name__}([{", ".join(map(repr, self))}], p={self.p})'
+
+    def _iter_blocks(self, level):
+        SPACING = 5
+
+        yield f'HEAD{"-" * (SPACING + 2) * (self._head.skips[level] - 1)}->'
+
+        current = self._head.forward_links[level]
+        while current is not TAIL:
+            yield f'{current.value!r:^{SPACING}}{"-" * (SPACING + 2) * (current.skips[level] - 1)}->'
+            current = current.forward_links[level]
+
+        yield f'TAIL'
+
+    def __str__(self):
+        return '\n'.join(''.join(self._iter_blocks(level)) for level in reversed(range(self.max_level)))
