8ebf3c306f16f9d692367988c45e46476c2a985e
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-05 20:43:12 -0500

    `remove` implemented.
    `Column` needed import.

--- sacks/sequences/sorted_list.py
+++ sacks/sequences/sorted_list.py
@@ -1,21 +1,24 @@
 from bisect import bisect_left, bisect_right, insort
 from collections.abc import MutableSet, Sequence
-from math import ceil, log2
+from math import ceil, log2, inf
 
-def pair_sum(iterable):
-    """Generate sum of consecutive pairs from iterable.
+from . import Column
+
+def pair_sum(iterator):
+    """Generate sum of consecutive pairs from iterator.
     """
-    for item in (it := iter(iterable))
-        yield item + next(it)
+    while (i := next(iterator, inf) + next(iterator, inf)) != inf:
+        yield i
 
 
 class SortedList(MutableSet, Sequence):
-    __slots__ = '_lists', '_weights', '_len', '_load',
+    __slots__ = '_lists', '_maxes', '_weights', '_len', '_load',
 
     DEFAULT_LOAD = 10
 
     def __init__(self, iterable=(), *, load=None):
         self._lists = []
+        self._maxes = Column(-1, self._lists)
         self._weights = []
 
         self._len = 0
@@ -27,17 +30,17 @@ class SortedList(MutableSet, Sequence):
         return self._len
 
     def __contains__(self, item):
+        if not self:
+            return False
+
         lists = self._lists
 
-        if not (maxes := _MaxView(lists)):
+        i = bisect_left(self._maxes, item)
+
+        if i == len(lists):
             return False
 
-        i = bisect_left(maxes, item)
-
-        if i == len(maxes):
-            return False
-
-        j = bisect_left(lists[j], item)
+        j = bisect_left(lists[i], item)
 
         return lists[i][j] == value
 
@@ -52,17 +55,16 @@ class SortedList(MutableSet, Sequence):
     def add(self, item):
         lists = self._lists
 
-        if maxes := _MaxView(lists):
-            i = bisect_right(maxes, item)
+        if lists:
+            i = bisect_right(self._maxes, item)
 
-            if i == len(maxes):
+            if i == len(lists):
                 i -= 1
                 lists[i].append(item)
             else:
                 insort(lists[i], item)
 
             self._expand(i)
-
         else:
             lists.append( [item] )
 
@@ -71,7 +73,6 @@ class SortedList(MutableSet, Sequence):
     def _expand(self, i):
         load = self._load
         lists = self._lists
-        weights = self._weights
 
         if len(lists[i]) > load << 1:
             sub = lists[i]
@@ -80,17 +81,56 @@ class SortedList(MutableSet, Sequence):
             del sub[load:]
 
             lists.insert(i + 1, half)
+            self._weights.clear()
 
-        if len(lists) <= len(weights) >> 1:
-            i -= len(weights)
-            while i < -2:
-                weights[i] += 1
-                i >>= 1
         else:
-            weights.clear()
+            self._weight_update(i, 1)
 
     def remove(self, item):
-        ...
+        if not self:
+            raise KeyError(item)
+
+        lists = self._lists
+
+        i = bisect_left(self._maxes, item)
+
+        if i == len(lists):
+            raise KeyError(item)
+
+        j = bisect_left(lists[i], item)
+
+        if lists[i][j] != item:
+            raise KeyError(item)
+
+        del lists[i][j]
+        self._shrink(i)
+        self._len -= 1
+
+    def _shrink(self, i):
+        load  = self._load
+        lists = self._lists
+
+        if len(lists[i]) < load >> 1 and len(lists) > 1:
+            i = min(i, 1)
+
+            lists[i - 1].extend(lists[i])
+
+            del lists[i]
+            self._weights.clear()
+
+            self._expand(i - 1)
+        else:
+            self._weight_update(i, -1)
+
+    def _weight_update(self, i, delta):
+        weights = self._weights
+        if not weights:
+            return
+
+        i -= len(weights)
+        while i != -1:
+            weights[i] += delta
+            i >>= 1
 
     def discard(self, item):
         try:
@@ -98,27 +138,45 @@ class SortedList(MutableSet, Sequence):
         except KeyError:
             pass
 
-    def __getitem__(self, i):
-        if not isinstance(i, int):
-            raise KeyError(i)
+    def clear(self):
+        self._lists.clear()
+        self._weights.clear()
 
-        _, i, _ = slice(i).indices(len(self))
-        if i >= len(self):
-            raise KeyError(i)
+    def __getitem__(self, index):
+        i, j = self._coord(index)
 
-        if not (weights := self._weights):
-            self._build_weights()
+        return self._lists[i][j]
+
+    def __delitem__(self, index):
+        i, j = self._coord(index)
+
+        del self._lists[i][j]
+        self._shrink(i)
+        self._len -= 1
+
+    def _coord(self, index):
+        if not isinstance(index, int):
+            raise KeyError(index)
+
+        _, j, _ = slice(index).indices(len(self))
+        if j >= len(self):
+            raise KeyError(index)
 
         weights = self._weights
-        node = -1
 
+        if not weights:
+            self._build_weights()
+
+        i = -1
         for _ in range(weights[-1]):
-            node <<= 1
-            if i >= weights[node]:
-                i -= weights[node]
-                node += 1
+            i <<= 1
+            if j >= weights[i]:
+                j -= weights[i]
+                i += 1
 
-        return self._lists[node][i]
+        i += len(weights)
+
+        return i, j
 
     def _build_weights(self):
         weights = self._weights
@@ -129,15 +187,14 @@ class SortedList(MutableSet, Sequence):
         n_lists = len(lists)
         height = ceil(log2(n_lists))
 
-
         base = list(map(len, lists))
         base.extend(0 for _ in range((1 << height) - n_lists))
 
         while base:
             weights.extend(base)
-            base = [a + b for a, b in chunk_by_2(base)]
+            base =tuple(pair_sum( iter(base) ))
 
         weights.append(height + 1)
 
     def __repr__(self):
-        return f'{type(self).__name__}([{", ".join(map(repr, self))}]'
+        return f'{type(self).__name__}([{", ".join(map(repr, self))}])'
