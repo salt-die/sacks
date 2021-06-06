b0d614016153be8326b62bda9a3ddda2070c0034
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-05 23:02:31 -0500

    More docs.
    Re-ordered methods:  public methods before private ones.

--- sacks/sequences/sorted_list.py
+++ sacks/sequences/sorted_list.py
@@ -28,9 +28,6 @@ class SortedList(MutableSet, Sequence):
 
         self |= iterable
 
-    def __len__(self):
-        return self._len
-
     def __contains__(self, item):
         if not self:
             return False
@@ -54,6 +51,9 @@ class SortedList(MutableSet, Sequence):
         for sublist in reversed(self._lists):
             yield from reversed(sublist)
 
+    def __len__(self):
+        return self._len
+
     def add(self, item):
         lists = self._lists
 
@@ -72,22 +72,6 @@ class SortedList(MutableSet, Sequence):
 
         self._len += 1
 
-    def _expand(self, i):
-        load = self._load
-        lists = self._lists
-
-        if len(lists[i]) > load << 1:
-            sub = lists[i]
-            half = sub[load:]
-
-            del sub[load:]
-
-            lists.insert(i + 1, half)
-            self._weights.clear()
-
-        else:
-            self._weight_update(i, 1)
-
     def remove(self, item):
         if not self:
             raise KeyError(item)
@@ -104,57 +88,13 @@ class SortedList(MutableSet, Sequence):
         if lists[i][j] != item:
             raise KeyError(item)
 
-        del lists[i][j]
-        self._shrink(i)
-        self._len -= 1
-
-    def _shrink(self, i):
-        load  = self._load
-        lists = self._lists
-
-        if len(lists[i]) < load >> 1 and len(lists) > 1:
-            i = min(i, 1)
-
-            lists[i - 1].extend(lists[i])
-
-            del lists[i]
-            self._weights.clear()
-
-            self._expand(i - 1)
-        else:
-            self._weight_update(i, -1)
-
-    def _weight_update(self, i, delta):
-        weights = self._weights
-        if not weights:
-            return
-
-        i -= len(weights)
-        while i != -1:
-            weights[i] += delta
-            i >>= 1
+        self._delete(i, j)
 
     def discard(self, item):
         try:
             self.remove(item)
         except KeyError:
             pass
-
-    def clear(self):
-        self._lists.clear()
-        self._weights.clear()
-
-    def __getitem__(self, index):
-        i, j = self._coord(index)
-
-        return self._lists[i][j]
-
-    def __delitem__(self, index):
-        i, j = self._coord(index)
-
-        del self._lists[i][j]
-        self._shrink(i)
-        self._len -= 1
 
     def pop(self, index=-1):
         """Remove and return item at index.
@@ -171,9 +111,93 @@ class SortedList(MutableSet, Sequence):
         try:
             return lists[i][j]
         finally:
-            del lists[i][j]
-            self._shrink(i)
-            self._len -= 1
+            self._delete(i, j)
+
+    def clear(self):
+        self._lists.clear()
+        self._weights.clear()
+
+    def index(self, item):
+        """Return first index of item `i`.
+        """
+        if not self:
+            raise KeyError(item)
+
+        lists = self._lists
+
+        i = bisect_left(self._maxes, item)
+
+        if i == len(lists):
+            raise ValueError(f'{item} is not in {type(self).__name__}')
+
+        j = bisect_left(lists[i], item)
+
+        if lists[i][j] != item:
+            raise ValueError(f'{item} is not in {type(self).__name__}')
+
+        return self._index(i, j)
+
+    def __getitem__(self, index):
+        i, j = self._coord(index)
+
+        return self._lists[i][j]
+
+    def __delitem__(self, index):
+        self._delete(*self._coord(index))
+
+    def _expand(self, i):
+        """Split sublist `i` if its size is greater than double _load.
+        """
+        load = self._load
+        lists = self._lists
+
+        if len(lists[i]) > load << 1:
+            sub = lists[i]
+            half = sub[load:]
+
+            del sub[load:]
+
+            lists.insert(i + 1, half)
+            self._weights.clear()
+
+        else:
+            self._weight_update(i, 1)
+
+    def _shrink(self, i):
+        """Combine sublist 'i' if its size is less than half _load.
+        """
+        lists = self._lists
+
+        if len(lists[i]) < self._load >> 1 and len(lists) > 1:
+            i = min(i, 1)
+
+            lists[i - 1].extend(lists[i])
+
+            del lists[i]
+            self._weights.clear()
+
+            self._expand(i - 1)
+        else:
+            self._weight_update(i, -1)
+
+    def _delete(i, j):
+        """Delete item `j` in sublist `i`.
+        """
+        del self._lists[i][j]
+        self._shrink(i)
+        self._len -= 1
+
+    def _weight_update(self, i, delta):
+        """Increment weights of leaf `i` and all its ancestors in _weights by delta.
+        """
+        weights = self._weights
+        if not weights:
+            return
+
+        i -= len(weights)
+        while i != -1:
+            weights[i] += delta
+            i >>= 1
 
     def _coord(self, index):
         """Return the pair (i, j) such that `_lists[i][j]` has given index.
@@ -201,17 +225,22 @@ class SortedList(MutableSet, Sequence):
 
         return i, j
 
+    def _index(self, i, j):
+        """Inverse of `_coord`. Return index of _lists[i][j].
+        """
+        return sum(len(self._lists[k]) for k in range(i)) + j
+
     def _build_weights(self):
         """
-        Each length of a list in _lists is a leaf in a binary-tree (padded with 0s until the number of
+        Each length of a list in _lists is a leaf in a binary-tree (padded with `0`s until the number of
         leaves is equal to the nearest power of 2). Parent nodes are added between consecutive leaves with
         weight equal to the sum of their children.  Similarly, parents of consecutive parents are added,
         until a single root is reached.
 
         The last item of `_weights` is the level of the tree.
 
-        Nodes on level i start at index -1 << i + 1. (Level 0 is the root with weight equal to length of the
-        sorted list.)
+        Nodes on level i start at index -1 << i + 1. (Level 0 at index -2 is the root with weight equal to
+        length of the sorted list.)
 
         """
         weights = self._weights
