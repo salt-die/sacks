c5f2e232b2e80c82090a326e815005bb0fc3886c
salt-die <53280662+salt-die@users.noreply.github.com>
2021-06-12 04:39:56 -0500

    Clarification in the docstring of `heappush_max'.

--- sacks/heaps/median_heap.py
+++ sacks/heaps/median_heap.py
@@ -6,7 +6,7 @@ from heapq import (
 )
 
 def heappush_max(heap, item):
-    """Push item onto heap, maintaining the heap invariant.
+    """Push item onto a maxheap, maintaining the heap invariant.
     """
     heap.append(item)
     _siftdown_max(heap, 0, len(heap) - 1)
